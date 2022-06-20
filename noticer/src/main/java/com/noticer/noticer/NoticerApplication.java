181eeac16d136c96ffa28ac732ebf8b68a2da378
daecheol.choi <cdc9308@naver.com>
2022-06-20 21:17:23 +0900

    feat: initialize project

---
+++ noticer/src/main/java/com/noticer/noticer/NoticerApplication.java
@@ -0,0 +1,13 @@
+package com.noticer.noticer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticerApplication.class, args);
+	}
+
+}
---
+++ noticer/src/test/java/com/noticer/noticer/NoticerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noticer.noticer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
