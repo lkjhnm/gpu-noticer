4d13f8d627bfe7a5c4ca970cfa700a93b079e4b6
daecheol.choi <daecheol.choi@epapyrus.com>
2022-07-09 16:12:34 +0900

    refact: 패키지 네이밍 수정

--- noticer/src/main/java/cdc/toy/noticer/boot/NoticerApplication.java
+++ noticer/src/main/java/cdc/toy/noticer/boot/NoticerApplication.java
@@ -1,4 +1,4 @@
-package com.noticer.noticer;
+package cdc.toy.noticer.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- noticer/src/test/java/cdc/toy/noticer/boot/NoticerApplicationTests.java
+++ noticer/src/test/java/cdc/toy/noticer/boot/NoticerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.noticer.noticer;
+package cdc.toy.noticer.boot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
